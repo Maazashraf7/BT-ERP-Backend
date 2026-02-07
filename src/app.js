2c79e5a57aeacfd82b341d066bcf5497a75e648b
Nitin Chauhan <147541211+supernitin06@users.noreply.github.com>
2026-02-07 10:50:01 +0530

    update main

--- src/app.js
+++ src/app.js
@@ -3,11 +3,19 @@ import cors from "cors";
 
 // ----------------------------
 // 👑 Super Admin (Platform)
+import platformDashboardRoutes from "./platform/dashboard/dashboard.routes.js";
+import platformAuditRoutes from "./platform/audit/audit.routes.js";
+import modulesRoutes from "./platform/modules/module.routes.js";
 
-import superAdminAuthRoutes from "./modules/superadminpower/superadmin.routes.js";
+
+import superAdminAuthRoutes from "./modules/admin/management/management.routes.js";
+import superAdminTenantRoutes from "./modules/admin/tenants/tenant.routes.js";
+import Subscription_Plan from "./modules/admin/plans/plan.routes.js";
 import { loginTenant } from "./modules/admin/tenants/tenant.controller.js";
 
 
+import permissionRoutes from "./modules/admin/permissions/permission.routes.js"; // Shared or specific?
+import featureRoutes from "./modules/admin/features/features.route.js";
 
 // 🏫 Tenant Admin
 import tenantRouter from "./modules/tenant.routes.js";
@@ -136,9 +144,24 @@ const API_V1 = "/api/v1";
 // -----------------------------
 // 👑 SUPER ADMIN (PLATFORM)
 // -----------------------------
-app.use(`${API_V1}/super-admin`, superAdminAuthRoutes); // Login/Management
+app.use(`${API_V1}/super-admin/auth`, superAdminAuthRoutes); // Login/Management
+app.use(`${API_V1}/super-admin/tenants`, superAdminTenantRoutes);
+app.use(`${API_V1}/super-admin/subscription`, Subscription_Plan);
+app.use(`${API_V1}/super-admin/features`, featureRoutes);
 
 
+
+
+app.use(`${API_V1}/super-admin/permissions`, permissionRoutes); // Permissions CRUD
+app.use(`${API_V1}/super-admin/dashboard`, platformDashboardRoutes);
+app.use(`${API_V1}/audit-logs`, platformAuditRoutes);
+app.use(`${API_V1}/super-admin/modules`, modulesRoutes);
+
+
+// -----------------------------
+// 🏫 TENANT DYNAMIC ROUTES
+// -----------------------------
+// Mounts all tenant functionality under /api/v1/:tenantName/
 app.post(`${API_V1}/auth/tenant/login`, loginTenant);
 app.use(`${API_V1}/:tenantName`, tenantRouter);
 // -----------------------------
