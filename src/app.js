6f4b8d09a956b257ce8ee6150a48082b66cc5a12
Nitin Chauhan <147541211+supernitin06@users.noreply.github.com>
2026-02-09 15:32:23 +0530

    update main

--- src/app.js
+++ src/app.js
@@ -181,6 +181,11 @@ app.use(`${API_V1}/super-admin/auth`, superAdminAuthRoutes); // Login/Management
 app.use(`${API_V1}/super-admin/tenants`, superAdminTenantRoutes);
 app.use(`${API_V1}/super-admin/subscription`, Subscription_Plan);
 app.use(`${API_V1}/super-admin/features/domain`, featureDomainRoutes);
+
+
+
+
+
 app.use(`${API_V1}/super-admin/features`, featureRoutes);
 app.use(`${API_V1}/super-admin/permissions`, permissionRoutes); // Permissions CRUD
 app.use(`${API_V1}/super-admin/tenant-features`, tenantsfeaturesRoutes);
