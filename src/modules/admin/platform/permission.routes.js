058c42c20864d5b762d40666006fffc85f7adc97
Nitin Chauhan <147541211+supernitin06@users.noreply.github.com>
2026-02-13 11:21:39 +0530

    update

--- src/modules/admin/platform/permission.routes.js
+++ src/modules/admin/platform/permission.routes.js
@@ -7,7 +7,8 @@ import {
     createPlatformPermissionDomain,
     listPlatformPermissionDomains,
     updatePlatformPermissionDomain,
-    deletePlatformPermissionDomain
+    deletePlatformPermissionDomain,
+    assignDomainToPermission
 } from "./permission.controller.js";
 import { authMiddleware } from "../../../core/middlewares/auth.middleware.js";
 import { requirePermission } from "../../../core/middlewares/permission.middleware.js";
@@ -23,10 +24,10 @@ router.get("/", requirePermission("VIEW_PERMISSIONS"), listPlatformPermissions);
 router.post("/assign/:roleId", requirePermission("ASSIGN_PERMISSIONS"), assignPermissionsToPlatformRole);
 
 // Platform Permission Domains
+router.post("/assign-domain", requirePermission("ASSIGN_DOMAIN"), assignDomainToPermission);
 router.post("/domains", requirePermission("CREATE_PERMISSION_DOMAIN"), createPlatformPermissionDomain);
 router.get("/domains", requirePermission("VIEW_PERMISSION_DOMAINS"), listPlatformPermissionDomains);
 router.put("/domains/:id", requirePermission("UPDATE_PERMISSION_DOMAIN"), updatePlatformPermissionDomain);
 router.delete("/domains/:id", requirePermission("DELETE_PERMISSION_DOMAIN"), deletePlatformPermissionDomain);
-
 export default router;
 
