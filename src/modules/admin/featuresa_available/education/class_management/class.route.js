c830ef59adc06b0dbd1980bb17d58923e2273c3f
SUDHANSHU DUBEY <manudubey249@gmail.com>
2026-02-18 15:29:27 +0530

    classes required for the clas

--- src/modules/admin/featuresa_available/education/class_management/class.route.js
+++ src/modules/admin/featuresa_available/education/class_management/class.route.js
@@ -13,7 +13,7 @@ import { checkDomainInPlan } from "../../../../../core/middlewares/fetures.middl
 const router = Router();
 
 // Domain restriction
-router.use(checkDomainInPlan("CLASS"));
+router.use(checkDomainInPlan("CLASSES"));
 
 // 🏫 Class CRUD
 router.post("/", requirePermission("CREATE_CLASS"), createClass);
